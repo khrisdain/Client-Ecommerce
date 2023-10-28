2e067b7300e98fdba969c3fde415b1b9f1e0435c
khrisdain <samuelilesanmi15@gmail.com>
2023-10-28 19:51:31 +0100

    Client-Ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+        
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
+ 
